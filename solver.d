9f9675b943890c7d9e3e08b792edc01246f89178
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-12-13 14:29:07 +0800

    Add constants

--- solver.d
+++ solver.d
@@ -1,6 +1,13 @@
 import std.stdio;
 
 void main() {
+	immutable two_blocks = 20;
+	immutable start_increase = 25;
+	immutable increase_increase = 10;
+	immutable clear_board = 2000;
+	immutable start_decrease = 20;
+	immutable decrease_increase = 40;
+
 	auto f = File("test.txt", "r");
 	foreach (row; f.byLine()) {
 		writeln(row);
