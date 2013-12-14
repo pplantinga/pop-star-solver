224ffd1d50fb36fc9f8db3248d0fa3ec765228bc
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-12-14 10:25:12 +0800

    Store file as array

--- solver.d
+++ solver.d
@@ -7,9 +7,13 @@ void main() {
 	immutable clear_board = 2000;
 	immutable start_decrease = 20;
 	immutable decrease_increase = 40;
+	immutable board_size = 10;
 
+	char[board_size][board_size] board;
 	auto f = File("test.txt", "r");
+	int i = 0;
 	foreach (row; f.byLine()) {
-		writeln(row);
+			board[i++] = row;
 	}
+	writeln(board);
 }
