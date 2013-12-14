db34cebb61e993d2e9db54387d53e68b6b64ae90
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-12-14 11:00:58 +0800

    add print and solve functions

--- solver.d
+++ solver.d
@@ -1,6 +1,7 @@
 import std.stdio;
 
-void main() {
+void main()
+{
 	immutable two_blocks = 20;
 	immutable start_increase = 25;
 	immutable increase_increase = 10;
@@ -12,8 +13,28 @@ void main() {
 	char[board_size][board_size] board;
 	auto f = File("test.txt", "r");
 	int i = 0;
-	foreach (row; f.byLine()) {
-			board[i++] = row;
+	foreach ( row; f.byLine() )
+	{
+		board[i++] = row;
 	}
-	writeln(board);
+
+	// Pretty print
+	void print_board( char[board_size][board_size] board )
+	{
+		foreach ( row; board )
+			writeln( row );
+	}
+
+	// See initial position
+	print_board( board );
+
+	// Solve function
+	int solve( char[board_size][board_size] board, int points )
+	{
+		return 0;
+	}
+
+	// Solve board
+	int points = solve( board, 0 );
+	writeln( points );
 }
